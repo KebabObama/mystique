ba19cd604212a5403e4ba7bcdb905a0668803b3b
KebabObama <lucaschyba@gmail.com>
2025-10-15 10:11:22 +0200

    emptied branch

--- lib/auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { user } from "@/db/schema";
-import { createAuthClient } from "better-auth/react";
-
-export type User = typeof user.$inferSelect;
-
-export const authClient = createAuthClient();
--- lib/mer.ts
+++
@@ -1,8 +0,0 @@
-import { Draft, produce } from "immer";
-
-export const mer =
-  <S>(mutator: (draft: Draft<S>) => void) =>
-  (prev: S): S =>
-    produce(prev, (draft: Draft<S>) => {
-      mutator(draft);
-    });
