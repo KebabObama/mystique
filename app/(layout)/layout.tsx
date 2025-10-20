ba86e58f2cb41bfcfbd55e7ef3afc1f131008e92
KebabObama <lucaschyba@gmail.com>
2025-10-20 20:42:35 +0200

    removal of useUser hook

--- app/(layout)/layout.tsx
+++ app/(layout)/layout.tsx
@@ -1,14 +1,11 @@
 import { SidebarApp } from "@/components/layout/sidebar-app";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { UserProvider } from "@/hooks/use-user";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <UserProvider>
-      <SidebarProvider className="h-dvh w-dvw overflow-hidden">
-        <SidebarApp />
-        <main className="relative h-full w-full">{children}</main>
-      </SidebarProvider>
-    </UserProvider>
+    <SidebarProvider className="h-dvh w-dvw overflow-hidden">
+      <SidebarApp />
+      <main className="relative h-full w-full">{children}</main>
+    </SidebarProvider>
   );
 }
