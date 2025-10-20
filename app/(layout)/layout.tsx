573a476e65731b4097a1e247dd01f86d4446f772
KebabObama <lucaschyba@gmail.com>
2025-10-20 20:41:06 +0200

    add userProvider to layout

--- app/(layout)/layout.tsx
+++ app/(layout)/layout.tsx
@@ -1,11 +1,14 @@
 import { SidebarApp } from "@/components/layout/sidebar-app";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { UserProvider } from "@/hooks/use-user";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider className="h-dvh w-dvw overflow-hidden">
-      <SidebarApp />
-      <main className="relative h-full w-full">{children}</main>
-    </SidebarProvider>
+    <UserProvider>
+      <SidebarProvider className="h-dvh w-dvw overflow-hidden">
+        <SidebarApp />
+        <main className="relative h-full w-full">{children}</main>
+      </SidebarProvider>
+    </UserProvider>
   );
 }
