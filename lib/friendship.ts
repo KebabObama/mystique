f6b37ba3677329805768a712fea3189762d7cc55
KebabObama <lucaschyba@gmail.com>
2025-11-06 13:05:15 +0100

    add friendship to socket

--- lib/friendship.ts
+++ lib/friendship.ts
@@ -1,59 +1,78 @@
-import { eq, or } from "drizzle-orm";
+"use server";
+
+import { arrayContains, eq, or } from "drizzle-orm";
 import { db } from "@/db";
 import * as schema from "@/db/schema";
 
-export namespace friendshipCRUD {
-	export const getAll = async (userId: string) => {
-		await db
-			.select({
-				friendshipId: schema.friendship.id,
-				state: schema.friendship.state,
-				sender: {
-					id: schema.user.id,
-					name: schema.user.name,
-					email: schema.user.email,
-					image: schema.user.image,
-				},
-				receiver: {
-					id: schema.user.id,
-					name: schema.user.name,
-					email: schema.user.email,
-					image: schema.user.image,
-				},
-			})
-			.from(schema.friendship)
-			.leftJoin(
-				schema.user,
-				or(
-					eq(schema.user.id, schema.friendship.sender),
-					eq(schema.user.id, schema.friendship.receiver),
-				),
-			)
-			.where(
-				or(
-					eq(schema.friendship.sender, userId),
-					eq(schema.friendship.receiver, userId),
-				),
-			);
-	};
-
-	export const send = async (sender: string, receiver: string) => {
-		await db.insert(schema.friendship).values({
-			sender,
-			receiver,
-		});
-	};
-
-	export const accept = async (friendshipId: string, state: number) => {
-		if (state !== -1) {
+export namespace CRUD {
+	export namespace friendship {
+		export const getAll = async (userId: string) => {
 			await db
-				.update(schema.friendship)
-				.set({ state })
-				.where(eq(schema.friendship.id, friendshipId));
-		} else {
-			await db
-				.delete(schema.friendship)
-				.where(eq(schema.friendship.id, friendshipId));
-		}
-	};
+				.select({
+					friendshipId: schema.friendship.id,
+					state: schema.friendship.state,
+					sender: {
+						id: schema.user.id,
+						name: schema.user.name,
+						email: schema.user.email,
+						image: schema.user.image,
+					},
+					receiver: {
+						id: schema.user.id,
+						name: schema.user.name,
+						email: schema.user.email,
+						image: schema.user.image,
+					},
+				})
+				.from(schema.friendship)
+				.leftJoin(
+					schema.user,
+					or(
+						eq(schema.user.id, schema.friendship.sender),
+						eq(schema.user.id, schema.friendship.receiver),
+					),
+				)
+				.where(
+					or(
+						eq(schema.friendship.sender, userId),
+						eq(schema.friendship.receiver, userId),
+					),
+				);
+		};
+
+		export const send = async (sender: string, receiver: string) => {
+			await db.insert(schema.friendship).values({
+				sender,
+				receiver,
+			});
+		};
+
+		export const accept = async (friendshipId: string, state: number) => {
+			if (state !== -1) {
+				await db
+					.update(schema.friendship)
+					.set({ state })
+					.where(eq(schema.friendship.id, friendshipId));
+			} else {
+				await db
+					.delete(schema.friendship)
+					.where(eq(schema.friendship.id, friendshipId));
+			}
+		};
+	}
+
+	export namespace message {
+		export type Insert = typeof schema.message.$inferInsert;
+
+		export const getAll = async (userId: string) => {
+			return await db
+				.select()
+				.from(schema.message)
+				.where(arrayContains(schema.message.receivers, [userId]));
+		};
+
+		export const send = async (msg: Insert) => {
+			await db.insert(schema.message).values(msg);
+		};
+	}
 }
--- sockets.ts
+++ sockets.ts
@@ -1,8 +1,9 @@
 /** biome-ignore-all lint/suspicious/noExplicitAny: <permit any> */
 import "dotenv/config";
+import { randomUUID } from "node:crypto";
 import http from "node:http";
 import { type DefaultEventsMap, Server, type Socket as SOC } from "socket.io";
-import { friendshipCRUD } from "./lib/friendship";
+import { CRUD } from "./lib/friendship";
 
 export type Socket = SOC<
 	DefaultEventsMap,
@@ -28,26 +29,44 @@ const userRoom = (userId: string) => `user:${userId}`;
 
 io.on("connection", (socket) => {
 	socket.on("join", (userId) => {
+		console.log(`join: ${userId}`);
 		socket.join(userRoom(userId));
 	});
 
 	socket.on("select-friendships", async (userId) => {
-		const temp = await friendshipCRUD.getAll(userId);
+		const temp = await CRUD.friendship.getAll(userId);
 		io.to(userRoom(userId)).emit("get-friendships", temp);
 	});
 
 	socket.on("send-friendships", async (sender, receiver) => {
-		await friendshipCRUD.send(sender, receiver);
-		const temp = await friendshipCRUD.getAll(sender);
+		await CRUD.friendship.send(sender, receiver);
+		const temp = await CRUD.friendship.getAll(sender);
 		io.to(userRoom(sender)).emit("get-friendships", temp);
 		io.to(userRoom(receiver)).emit("get-friendships", temp);
 	});
 
 	socket.on("state-friendship", async (friendship, state) => {
-		await friendshipCRUD.accept(friendship.id, state);
-		const temp = await friendshipCRUD.getAll(friendship.sender);
+		await CRUD.friendship.accept(friendship.id, state);
+		const temp = await CRUD.friendship.getAll(friendship.sender);
 		io.to(userRoom(friendship.sender)).emit("get-friendships", temp);
 		io.to(userRoom(friendship.sender)).emit("get-friendships", temp);
+	});
+
+	socket.on("select-messages", async (userId: string) => {
+		const temp = await CRUD.message.getAll(userId);
+		io.to(userRoom(userId)).emit("get-messages", temp);
+	});
+
+	socket.on("send-message", async (msg) => {
+		await CRUD.message.send(msg);
+		const temp = {
+			...(msg as CRUD.message.Insert),
+			id: randomUUID(),
+			date: new Date(),
+		};
+		temp.receivers.forEach((e) => {
+			io.to(userRoom(e)).emit("receive-message", msg);
+		});
 	});
 });
 
