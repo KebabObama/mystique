5e710c67ecc98901d3457e32b73158e2045a59fe
KebabObama <lucaschyba@gmail.com>
2025-11-13 09:23:39 +0100

    add trigger

--- app/(layout)/(wrapper)/layout.tsx
+++ app/(layout)/(wrapper)/layout.tsx
@@ -1,6 +1,14 @@
+import Card from "@/components/ui/card";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
 export default ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="relative h-dvh w-full overflow-x-hidden overflow-y-auto px-6 py-4">
+		<div className="relative h-dvh w-full overflow-x-hidden overflow-y-auto space-y-6 px-6 py-4">
+			<Card>
+				<Card.Content>
+					<SidebarTrigger />
+				</Card.Content>
+			</Card>
 			{children}
 		</div>
 	);
