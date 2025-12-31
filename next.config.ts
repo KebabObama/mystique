63d0f5788f2236cadfd449f80973578a02a137ac
KebabObama <lucaschyba@gmail.com>
2025-12-31 18:52:13 +0100

    fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   experimental: { serverActions: { bodySizeLimit: "10mb" } },
   compress: true,
-  cacheComponents: true,
 };
 
 export default nextConfig;
