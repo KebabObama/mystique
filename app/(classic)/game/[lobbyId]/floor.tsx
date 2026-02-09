d67bf606c6464a12cba6354e4f365aa1bc411a91
KebabObama <lucaschyba@gmail.com>
2026-02-09 18:23:47 +0100

    stash: floor

--- app/(classic)/game/[lobbyId]/floor.tsx
+++ app/(classic)/game/[lobbyId]/floor.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import { useCamera } from "@/hooks/use-camera";
+import { useGame } from "@/hooks/use-game";
+import { useUser } from "@/hooks/use-user";
 import { Render } from "@/lib/render";
 import { Plane } from "@react-three/drei";
+import React from "react";
 
-export const Floor = () => {
+export const Floor = React.memo(() => {
   const target = useCamera((s) => s.camera.target);
+  const userId = useUser((s) => s?.id);
+  const instance = useGame((s) => s.instance);
+
   const position = Render.getTilePosition(target);
+  const isMaster = userId === instance?.id;
+  const isOnTurn = isMaster ? instance?.turn === -1 : false;
+
+  const handle = () => {
+    if (!isOnTurn) return;
+    if (isMaster) {
+    } else {
+    }
+  };
 
   return (
     <>
@@ -18,5 +33,5 @@ export const Floor = () => {
       <gridHelper args={[50, 50, "#444444"]} position={[position.x, position.y, position.z]} />
     </>
   );
-};
+});
 
