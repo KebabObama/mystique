09a0d762cd04ea3eaa79861cb314177076c8bc4f
KebabObama <lucaschyba@gmail.com>
2026-02-26 11:50:02 +0100

    deny bun to support prettier

--- components/inventory/inventory-panel.tsx
+++ components/inventory/inventory-panel.tsx
@@ -3,7 +3,7 @@
 import { StoragePanel } from "@/components/inventory/storage-panel";
 import { ViewPanel } from "@/components/inventory/view-panel";
 import { useGame } from "@/hooks/use-game";
-import { InventoryItem } from "@/lib/inventory-panel";
+import type { InventoryItem } from "@/lib/inventory-panel";
 import { MasterPanel } from "./master-panel";
 
 type InventoryPanelProps = { items: InventoryItem[] };
