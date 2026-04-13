de848ca073d2549ade14e66f4a49c0ce7414c30b
kebabObama <lucaschyba@gmail.com>
2026-04-13 15:32:52 +0200

    standalone

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: { serverActions: { bodySizeLimit: "4mb" } },
+  experimental: { serverActions: { bodySizeLimit: "10mb" } },
   compress: true,
   reactStrictMode: false,
+  output: "standalone",
 };
 
 export default nextConfig;
